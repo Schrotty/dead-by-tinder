b34e8987c348fef5fe5656d40d12d505a191f777
Ruben Maurer <rubenmaurer@live.de>
2020-05-09 19:28:53 +0200

    Updates

---
+++ precache-manifest.5e4f7eee92965693673166cd4e187846.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "270d0e0bf818e785d9ca8e27186c5e86",
+    "url": "/dead-by-tinder/index.html"
+  },
+  {
+    "revision": "cce355553cd234cadc58",
+    "url": "/dead-by-tinder/static/css/main.f27c0eb1.chunk.css"
+  },
+  {
+    "revision": "1a59fa5326dc7f9887a0",
+    "url": "/dead-by-tinder/static/js/2.6422b117.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dead-by-tinder/static/js/2.6422b117.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cce355553cd234cadc58",
+    "url": "/dead-by-tinder/static/js/main.7677f965.chunk.js"
+  },
+  {
+    "revision": "effe67163928cfdc0289",
+    "url": "/dead-by-tinder/static/js/runtime-main.18d614d2.js"
+  }
+]);
